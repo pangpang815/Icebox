73c2fea6be6b8f473b1bb3ff10a397a44d2cfabb
pangpang815 <pangpang815@gmail.com>
2012-08-08 18:56:52 +0900

    css 추가

--- webapp/common/footer/footer.php
+++ webapp/common/footer/footer.php
@@ -1,3 +1,28 @@
-<?php
-
-?>
+<!-- UI Object -->
+<h2 style="margin:20px;color:#333;font-family:NanumGothic;font-size:16px;font-weight:bold;line-height:16px;letter-spacing:-1px">01. 기본형</h2>
+<!-- Footer -->
+<div id="footer">
+	<ul>
+	<li class="frst">
+		<a href="#" target="_blank">카페 도움말</a></li>
+	<li>
+		<a href="#" target="_blank">카페 이용약관</a></li>
+	<li>
+		<a href="#" target="_blank">카페 운영원칙</a></li>
+	<li>
+		<a href="#" target="_blank"><strong>개인정보취급방침</strong></a></li>
+	<li>
+		<a href="#" target="_blank">책임의 한계와 법적고지</a></li>
+	<li>
+		<a href="#" target="_blank">게시중단요청서비스</a></li>
+	<li>
+		<a href="#" target="_blank">고객센터</a></li>
+	</ul>
+	<address>
+		<a href="#" target="_blank" class="logo"><img src="http://static.naver.com/common/footer/logo_nhn.gif" width="62" height="15" alt="NHN"></a>
+		<em>Copyright ©</em>
+		<a href="#" target="_blank">NHN Corp.</a>
+		<span>All Rights Reserved.</span>
+	</address>
+</div>
+<!-- //Footer -->
