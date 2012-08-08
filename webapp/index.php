9dd05b1148821ae22b6433a9e8631aa24d9022f8
pangpang815 <pangpang815@gmail.com>
2012-08-09 01:15:01 +0900

    server 설정 추가

--- webapp/index.php
+++ webapp/index.php
@@ -1,7 +1,18 @@
+<!DOCTYPE html>
 <html>
-	<body>
-	<h2> PHP Project start!! </h2>
+	<head>
+		<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+		<meta http-equiv="Content-Script-Type" content="text/javascript" />
+		<meta http-equiv="Content-Style-Type" content="text/css" />
+		<meta http-equiv="X-UA-Compatible" content="IE=edge" />
 	
-	<?php include "common/footer/footer.php" ?>
+	  <!--  <link rel="shortcut icon" type="image/x-icon" href="http://static.naver.net/www/favicon.ico" />  -->
+		
+		<link rel = "stylesheet" type = "text/css" href = "../css/common.css">
+	</head>
+	<body>
+		<h2> PHP Project start!! </h2>
+		
+		<?php include "common/footer/footer.php" ?>
 	</body>
 </html>
