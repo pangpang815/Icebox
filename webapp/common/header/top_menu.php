a02b7469041288247deaef6118a9e26dd8f9a64c
pangpang815 <pangpang815@gmail.com>
2012-08-10 19:01:53 +0900

    Top menu 추가

---
+++ webapp/common/header/top_menu.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html>
+	<body>
+		<div class="top-menu">
+			<div class="top-bar-left"></div>
+			<div class="top-bar"></div>
+			<div class="top-bar-right"></div>
+			<div class="message">뛰어난 보냉력을 자랑하는 휴가철 필수품 </div>
+		
+			<div class="menu">
+				<a class="item support" torollover="support" href="">SUPPORT
+					<div class="arrow-container"><div class="arrow"></div></div>
+				</a>
+				<a class="item signin last" torollover="signin" href="">LOG IN
+					<div class="arrow-container"><div class="arrow"></div></div>
+				</a>
+			</div>
+		</div>
+			
+		<div class="main-menu">
+			<a class="logo" href=""><img alt="Daewon" width="200" height="80" src="../images/logo.png"> </a>
+			<div class="menu futura">
+				<a class="item item1" href="">About</a>
+				<a class="item item2" href="">Product</a>
+				<a class="item item3" href="">Contact</a>
+				<a class="item item4" href="">Notice</a>
+				<a class="item item5" href="">RoadMap</a>
+			</div>
+		</div>
+		
+	</body>
+</html>
--- webapp/index.php
+++ webapp/index.php
@@ -1,35 +1,26 @@
 <!DOCTYPE html>
 <html>
 	<head>
+		<title>대원산업</title>	
 		<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
 		<meta http-equiv="Content-Script-Type" content="text/javascript" />
 		<meta http-equiv="Content-Style-Type" content="text/css" />
 		<meta http-equiv="X-UA-Compatible" content="IE=edge" />
-	
+		
 	  <!--  <link rel="shortcut icon" type="image/x-icon" href="http://static.naver.net/www/favicon.ico" />  -->
 		<link rel = "stylesheet" type = "text/css" href = "../css/common.css">
+		<link rel="shortcut icon" href="../images/favicon.png">
 		<script src="../js/common.js" type="text/javascript"></script>
-			
 	</head>
 	<body class="home-page">
 		<div class="inner header"> 		
-			<div class="top-menu">
-				<div class="top-bar-left"></div>
-				<div class="top-bar"></div>
-				<div class="top-bar-right"></div>
-				<div class="message">뛰어난 보냉력을 자랑하는 휴가철 필수품 </div>
-				
-				<div class="menu">
-					<a class="item support" torollover="support" href="">SUPPORT
-						<div class="arrow-container"><div class="arrow"></div></div>
-					</a>
-					<a class="item signin last" torollover="signin" href="">SIGN IN
-						<div class="arrow-container"><div class="arrow"></div></div>
-					</a>
-				</div>
-			</div>
+			<?php include "common/header/top_menu.php" ?>
 		</div>
 		
+		<div class="billboard master-fader">
+			<div class="container container-fill fill-blue"></div>
+		</div>
+		<div class="pager"></div>
 		
 		<?php include "common/footer/footer.php" ?>
 	</body>
