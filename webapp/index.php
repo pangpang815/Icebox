e4b8802f3528a7df35f2827306c5121e8e53bd23
yun <yun@yun-PC>
2012-08-12 21:25:33 +0900

    추가

--- webapp/index.php
+++ webapp/index.php
@@ -40,8 +40,51 @@
 			  </div>
 			  </div>
 		</div>
-		<div class="pager"></div>
+
+		<div class="pager"><a href="#" class=""></a><a href="#" class=""></a><a href="#" class="activeSlide"></a></div>
 		
+		<div class="marketing">
+  <div class="grid-container">
+    <div class="grid">
+      <div class="g3-l1">
+        <a href="/products/editor-pick">
+          <img src="/assets/home/icon_glasses-22a4f73f246b1f2f2e7f7af1920bfaec.png">
+          <span class="title">Glasses<br>you’ll love.</span>
+        </a>
+        <div class="description">
+          With a huge variety of designer sunglasses and prescription glasses
+          starting at $109 and prescription lenses starting at $59, we’ve got
+          the pair that’s right for you.
+        </div>
+      </div>
+
+      <div class="g3-l2">
+        <a href="/tryon/doctora-espl04/C#ask-stylist">
+          <img src="/assets/button_ask_a_stylist-939c3381a4bd9f3e482fee02bfe64100.png">
+          <span class="title">Style advice<br>you’ll trust.</span>
+        </a>
+        <div class="description">
+          Send your DITTO to brutally honest friends who will tell you how you
+          look in those glasses. You can also ask our trained stylists for a
+          personal recommendation.
+        </div>
+      </div>
+
+      <div class="g3-l3">
+        <a href="/returns">
+          <img src="/assets/home/icon_present-90b9e934693598d4f39b85d7474b5383.png">
+          <span class="title">Service<br>you’ll remember.</span>
+        </a>
+        <div class="description">
+          Free shipping and 90-day free returns make shopping risk-free. We
+          also offer a 2-year warranty that covers everything from scratches
+          to you sitting on them in your car.
+        </div>
+      </div>
+    </div>
+  </div>
+</div>
+
 		<?php include "common/footer/footer.php" ?>
 	</body>
 </html>
